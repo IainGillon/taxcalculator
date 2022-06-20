7109b809a3dc6bd80f40b8a26163ba36d027a5b3
IainGillon <54671696+IainGillon@users.noreply.github.com>
2022-06-20 21:33:07 +0100

    began logic

---
+++ src/calculator.js
@@ -0,0 +1,68 @@
+// useState to set salary and pension
+// 
+
+
+let s = 0;
+
+
+function taxCalculator(){
+    let PAYE = 0;
+    
+    if(s <= 12570){
+        
+        PAYE = 0;
+          
+    }
+
+    else if(s > 12570 && s <=14732){
+        PAYE = s - 12570 / 100 * 19;
+    }
+
+    else if(s > 25689 && s <= 43662){
+        PAYE = s - 12570 / 100 * 20;
+    }
+
+    else if(s > 53662 && s <= 150000){
+        PAYE = s - 12570 / 100 * 40;
+    }
+
+    else{
+        PAYE = s - 12570 / 100 * 46;
+    }
+
+    return PAYE;
+
+
+}
+
+
+function pensionCalculator(p){
+
+    let pension = s / 100 * p;
+
+    return pension;
+
+}
+
+function niCalculator(){
+
+    let ni = 0
+
+    let t = s - 9568;
+
+
+    if(s <= 9568){
+
+        ni = 0;
+
+    }
+    else if(s > 9568 && s <= 50270 ){
+        ni = t / 100 * 13.25
+    }
+
+    else if(s > 50270){
+        
+        let income1 = t 
+
+    }
+}
