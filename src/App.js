6912f077ff088fdcef686c150623c00847f6b9db
IainGillon <54671696+IainGillon@users.noreply.github.com>
2022-06-21 12:33:49 +0100

    began logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
   return (
- 
+  <p>$$$</p>
   );
 }
 
---
+++ src/containers/taxCont.js
@@ -0,0 +1,5 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import Calculator from "../Logic/calculator";
+
+
